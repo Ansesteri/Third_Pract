3fd234fbd76b6a9b4aaa176d61dbde2642141a06
Ansesteri <angrylemon5678@gmail.com>
2022-10-15 15:39:57 +0300

    Add TaskV_8.py

---
+++ TaskV_8.py
@@ -0,0 +1,25 @@
+from ast import While
+
+
+stack = []
+
+while True:
+    comm = input().strip()
+    if comm.startswith('push'):
+        number = int(comm.split()[1])
+        stack.append(number)
+        print("ok")
+    elif comm == "pop":
+        print(stack.pop())
+    elif comm == "back":
+        print(stack[-1])
+    elif comm == "size":
+        print(len(stack))
+    elif comm == "clear":
+        stack.clear()
+        print("ok")
+    elif comm == "exit":
+        print("bye")
+        break
+    else:
+        print("error")
