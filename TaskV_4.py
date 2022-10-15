d1dfd314aea4621d28f9b4b7c4b8a68671933d5b
Ansesteri <angrylemon5678@gmail.com>
2022-10-15 13:54:13 +0300

    Update TaskV_4.py

--- TaskV_4.py
+++ TaskV_4.py
@@ -1,7 +1,12 @@
 N = int(input())
 students = {}
 marks = 3
+output = []
 for _ in range(N):
     student = input().split()
+    last_name = student[0]
     students[student[0]] = [int(i) for i in student[1:]]
-    print(f'{student[0]} {(sum(students[student[0]]) / marks):.2f}')
+    avg_mark = sum(students[student[0]]) / marks
+    output.append('%s %.2f' % (last_name, avg_mark))
+print('-' * 20)
+print('\n'.join(output))
