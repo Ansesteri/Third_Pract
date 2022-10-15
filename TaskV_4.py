756e0aadfd6b5c2a64d5ad610885dcac7d32fb09
Ansesteri <angrylemon5678@gmail.com>
2022-10-15 13:48:54 +0300

    Update TaskV_4.py

--- TaskV_4.py
+++ TaskV_4.py
@@ -1,7 +1,7 @@
 N = int(input())
 students = {}
 marks = 3
-for i in range(N):
+for _ in range(N):
     student = input().split()
     students[student[0]] = [int(i) for i in student[1:]]
-    print(f'{student[0]} {sum(students[student[0]])/marks}0')
+    print(f'{student[0]} {(sum(students[student[0]]) / marks):.2f}')
